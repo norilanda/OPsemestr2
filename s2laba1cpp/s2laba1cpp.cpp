398c9bc641a2dd18d79b62cba2764f1ad7cedcfb
norilanda <lidiamakarcuk9@gmail.com>
2022-02-07 23:29:23 +0200

    Execute rewriting strings into new file in main function

--- s2laba1cpp/s2laba1cpp.cpp
+++ s2laba1cpp/s2laba1cpp.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
     string pathOfFile = "file.txt";
+    string pathOfNewFile = "new file.txt";
     create_file(pathOfFile);
     cout << "Here is the first file:\n";
     print_file(pathOfFile);
+    rewrite_text_into_new_file(pathOfFile, pathOfNewFile);
+    cout << "The new file:\n";
+    print_file(pathOfNewFile);
 
 }
 
