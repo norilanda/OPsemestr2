3f76ebf32ff445d64e5029582dd48de282c459ba
norilanda <lidiamakarcuk9@gmail.com>
2022-02-08 09:15:25 +0200

    Enter text to out_file

--- s2laba1py/functions.py
+++ s2laba1py/functions.py
@@ -1,5 +1,7 @@
 def create_file(path):
     out_file=open(path,'wt')
     string_of_text=input("Enter your strings. To finish press 'Enter' twice\n")
-    
-
+    while string_of_text!='':
+        out_file.write(string_of_text)
+        string_of_text=input()
+    out_file.close()
