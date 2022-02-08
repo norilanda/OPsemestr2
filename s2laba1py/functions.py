8e18b45fc938a4bf7cbe453ba90d7a994e2d9318
norilanda <lidiamakarcuk9@gmail.com>
2022-02-08 09:30:06 +0200

    Display text in separate srtings

--- s2laba1py/functions.py
+++ s2laba1py/functions.py
@@ -2,12 +2,12 @@ def create_file(path):
     out_file=open(path,'wt')
     string_of_text=input("Enter your strings. To finish press 'Enter' twice\n")
     while string_of_text!='':
-        out_file.write(string_of_text)
+        out_file.write(string_of_text+'\n')
         string_of_text=input()
     out_file.close()
 
 def print_file(path):
     in_file=open(path,'rt')
     for string in in_file:
-        print(string)
+        print(string,end='')
     in_file.close()
--- s2laba1py/s2laba1py.py
+++ s2laba1py/s2laba1py.py
@@ -2,3 +2,5 @@ from functions import create_file, print_file
 
 pathOfFile = "file.txt"
 pathOfNewFile = "new file.txt"
+create_file(pathOfFile)
+print_file(pathOfFile)
