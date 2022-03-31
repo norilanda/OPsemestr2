3b4dec702b09d749bee215a0d084a47a778be05e
norilanda <lidiamakarcuk9@gmail.com>
2022-03-31 18:54:01 +0300

    Implement functions

--- Лабораторна №2/laba2cpp/laba2cpp.cpp
+++ Лабораторна №2/laba2cpp/laba2cpp.cpp
@@ -11,5 +11,9 @@ int main()
     write_automobiles_into_file(pathAutoList);
     cout << "\nThere is information in file:\n\n";
     display_file_information(pathAutoList);
+    write_new_file_of_automobile(pathAutoList, newPathAutoList);
+    cout << "-----------------------------------------------------\n\n";
+    cout << "There is a list of automobiles that have been sold within 2 months after production:\n";
+    display_file_information(newPathAutoList);
 }
 
