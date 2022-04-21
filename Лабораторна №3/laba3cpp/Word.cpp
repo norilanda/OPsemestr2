460cc00e669217c825666ef6520c4ca71dced998
norilanda <lidiamakarcuk9@gmail.com>
2022-04-21 17:21:20 +0300

    Create functions

--- Лабораторна №3/laba3cpp/Word.cpp
+++ Лабораторна №3/laba3cpp/Word.cpp
@@ -1,1 +1,49 @@
 #include "Word.h"
+using namespace std;
+
+Word::Word(string word)
+{ 
+	this->word = word;
+}
+string Word::get_word() 
+{ 
+	return word; 
+}
+int Word::calc_digits()
+{
+	int digits = 0;
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (isdigit(word[i]))
+			digits++;
+	}
+	return digits;
+}
+bool is_word(string str)
+{
+	bool isWord = true;
+	int i = 0;
+	while (isWord && i < str.length())
+	{
+		if (!isalnum(str[i]))
+			isWord = false;
+	}
+	return isWord;
+}
+vector<Word> create_arr(int n)
+{
+	vector <Word> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Enter word: ";
+		string str; cin >> str;
+		while (!is_word(str))
+		{
+			cout << "You can enter only letters or digits. Try again: ";
+			cin >> str;
+		}
+		arr[i] = Word(str);
+	}
+}
+void print_arr(vector<Word>arr);
+int word_with_max_number_of_digits(vector<Word>arr);
--- Лабораторна №3/laba3cpp/Word.h
+++ Лабораторна №3/laba3cpp/Word.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+// розробити клас слово. створити масив обєктів даного типу.визначити слово з max к-стю цифр
+class Word
+{
+	string word;
+public:
+	Word(string word);
+	string get_word();
+	int calc_digits();
+	//перевірити чи слово є словом
+};
+bool is_word(string str);
+vector<Word> create_arr(int n);
+void print_arr(vector<Word>arr);
+int word_with_max_number_of_digits(vector<Word>arr);
