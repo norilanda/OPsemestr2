4fc66d62d61dc4a741d22275a936df4e4538c991
norilanda <lidiamakarcuk9@gmail.com>
2022-04-30 23:03:45 +0300

    Add attributes

--- Лабораторна №4/laba4cpp/Polynomial.h
+++ Лабораторна №4/laba4cpp/Polynomial.h
@@ -1,6 +1,16 @@
 #pragma once
 
+//конструктори, гетери, метод обчислення значення в конкретній точці
 class Polynomial
 {
+	double c0;
+	double c1;
+	double c2;
+	double c3;
+public:
+	//конструктор
 
+	//гетери
+
+	double calculate_polynomial(double x);
 };
