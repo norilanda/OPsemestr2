f2202e704f63f63bc4bd12a02bf4717ef4ed6bcf
Лидия Макарчук <lidiamakarcuk9@gmail.com>
2022-05-20 11:02:32 +0300

    Create constructors for TVector

--- Лабораторна №5/laba5cpp/TVector.cpp
+++ Лабораторна №5/laba5cpp/TVector.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <cmath>
 #include "TVector.h"
 
 using namespace std;
+
+TVector::TVector(double x)
+{
+	this->x = x;
+}
+TVector::TVector(int rangeMin, int rangeMax)
+{
+	x = generate_number(rangeMin, rangeMax);
+}
+int TVector::generate_number(int rangeMin, int rangeMax)
+{
+	int number = ((double)rand() / RAND_MAX) * (rangeMax - rangeMin) + rangeMin;
+	return number;
+}
--- Лабораторна №5/laba5cpp/TVector.h
+++ Лабораторна №5/laba5cpp/TVector.h
@@ -1,1 +1,15 @@
 #pragma once
+
+class TVector
+{
+protected:
+	double x;
+	static int generate_number(int rangeMin, int rangeMax);
+public:
+	//constructors
+	TVector(double x);
+	TVector(int rangeMin = -5, int rangeMax = 5);
+	// паралельн≥сть
+	// перпендикул€рн≥сть
+	// метод визначенн€ довжини
+};
