d092e0a30b258b06213216d63ae12fd802171cc9
Лидия Макарчук <lidiamakarcuk9@gmail.com>
2022-05-21 09:41:07 +0300

    Delete extra

--- Лабораторна №5/laba5cpp/laba5cpp.cpp
+++ Лабораторна №5/laba5cpp/laba5cpp.cpp
@@ -26,15 +26,4 @@ int main()
     cout << "\nVectors which are perpendicular to the first vector:\n";
     display_vector(arrPerp);
     cout << "\nSum of R2 vectors, which are perpendicular = " << perpendicular << "\n";
-
-    /*R3 r(1.0, 0.0, -1.0);
-    R3 r1(2.0, 8.0, 2.0);
-    r.display(); cout << "\n";
-    r1.display();
-    TVector* basePtr;
-    basePtr = &r1;
-
-    cout << r1.calc_length() << "\n";
-    cout << r.is_parallel(basePtr) << "\n";
-    cout << r.is_perpendicular(basePtr) << "\n";*/
 }
