604f0aa8f2c3ea055525032cc17d195722c9a942
Лидия Макарчук <lidiamakarcuk9@gmail.com>
2022-05-21 11:20:52 +0300

    Change output information

--- Лабораторна №5/laba5cpp/laba5cpp.cpp
+++ Лабораторна №5/laba5cpp/laba5cpp.cpp
@@ -18,12 +18,12 @@ int main()
     display_arr_of_vectors(arr, n, m);
 
     double parallelSum = calc_sum_of_parallels(arr, n, m, arrPar);
-    cout << "\nVectors which are parallel to the first vector:\n";
+    cout << "\nVectors which are parallel to the first R2 vector:\n";
     display_vector(arrPar);
     cout << "\nSum of R2 vectors, which are parallel = " << parallelSum << "\n";
     
     double perpendicular = calc_sum_of_perpendicular(arr, n, m, arrPerp);
-    cout << "\nVectors which are perpendicular to the first vector:\n";
+    cout << "\nVectors which are perpendicular to the first R3 vector:\n";
     display_vector(arrPerp);
-    cout << "\nSum of R2 vectors, which are perpendicular = " << perpendicular << "\n";
+    cout << "\nSum of R3 vectors, which are perpendicular = " << perpendicular << "\n";
 }
